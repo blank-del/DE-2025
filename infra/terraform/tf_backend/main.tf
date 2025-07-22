779343536708901da38acc5650db451bae6897d0
Hassan Majeed <36446500+blank-del@users.noreply.github.com>
2025-07-22 22:26:31 +0300

    Posting workflow to UI

--- infra/terraform/tf_backend/main.tf
+++ infra/terraform/tf_backend/main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_resource_group" "tf_rg" {
-    name = var.tf_resource_group_name
-    location = var.location
+    name      = var.tf_resource_group_name
+    location  = var.location
 }
 
 resource "azurerm_storage_account" "tf_st" {
-    name = var.tf_storage_account_name
-    resource_group_name = azurerm_resource_group.tf_rg.name
-    location = azurerm_resource_group.tf_rg.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+    name                      = var.tf_storage_account_name
+    resource_group_name       = azurerm_resource_group.tf_rg.name
+    location                  = azurerm_resource_group.tf_rg.location
+    account_tier              = "Standard"
+    account_replication_type  = "LRS"
 }
 
 resource "azurerm_storage_container" "tf_state" {
